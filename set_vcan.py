60b1aa2c25369c6269d9149ac695604acd897c88
Vishwa Vasu <vasu@vazdefense.com>
2024-08-10 22:27:39 +0200

    Update set_vcan.py

--- set_vcan.py
+++ set_vcan.py
@@ -1,6 +1,5 @@
 import subprocess
 
-
 subprocess.call('sudo modprobe vcan', shell=True)
 subprocess.call('sudo ip link add dev vcan0 type vcan', shell=True)
 subprocess.call('sudo ip link set up vcan0', shell=True)
